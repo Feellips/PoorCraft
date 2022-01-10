a5c69b39bc14f800fe683c11e31ff27d9be268b1
~FeelLipss <32616629+Feellips@users.noreply.github.com>
2022-01-10 09:08:00 +0300

    Add project files.

---
+++ PoorCraft/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using OpenTK.Windowing.Desktop;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using OpenTK.Windowing.Common;
+using OpenTK.Mathematics;
+
+namespace PoorCraft
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new Vector2i(800, 600),
+                Title = "PoorCraft",
+                Flags = ContextFlags.ForwardCompatible,
+            };
+
+            using (var game = new Game(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                game.Run();
+            }
+        }
+    }
+}
---
+++ PoorCraft/Texture.cs
@@ -0,0 +1,133 @@
+﻿using OpenTK.Graphics.OpenGL;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Drawing;
+using System.Drawing.Imaging;
+using PixelFormat = OpenTK.Graphics.OpenGL.PixelFormat;
+using Image = SixLabors.ImageSharp.Image;
+
+namespace PoorCraft
+{
+    // A helper class, much like Shader, meant to simplify loading textures.
+    public class Texture
+    {
+        public readonly int Handle;
+
+        public static Texture LoadSpriteSheetFromFile(string path)
+        {
+            using var image = Image.Load<Rgba32>("Textures/" +path);
+
+            byte[] data = new byte[image.Width * image.Height * 4];
+            var i = 0;
+            for (var y = 0; y < image.Height; y++)
+                foreach (var p in image.GetPixelRowSpan(y))
+                {
+                    data[i++] = p.R;
+                    data[i++] = p.G;
+                    data[i++] = p.B;
+                    data[i++] = p.A;
+                }
+
+            int handle = GL.GenTexture();
+            GL.BindTexture(TextureTarget.Texture2D, handle);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba,
+                image.Width, image.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, data);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS,
+                (int)TextureWrapMode.ClampToEdge);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT,
+                (int)TextureWrapMode.ClampToEdge);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter,
+                (int)TextureMinFilter.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter,
+                (int)TextureMagFilter.Nearest);
+
+            return new Texture(handle);
+        }
+
+        public static Texture LoadFromFile(string path)
+        {
+            // Generate handle
+            int handle = GL.GenTexture();
+
+            // Bind the handle
+            GL.ActiveTexture(TextureUnit.Texture0);
+            GL.BindTexture(TextureTarget.Texture2D, handle);
+
+            // For this example, we're going to use .NET's built-in System.Drawing library to load textures.
+
+            // Load the image
+            using (var image = new Bitmap(path))
+            {
+                // Our Bitmap loads from the top-left pixel, whereas OpenGL loads from the bottom-left, causing the texture to be flipped vertically.
+                // This will correct that, making the texture display properly.
+                image.RotateFlip(RotateFlipType.RotateNoneFlipY);
+
+                // First, we get our pixels from the bitmap we loaded.
+                // Arguments:
+                //   The pixel area we want. Typically, you want to leave it as (0,0) to (width,height), but you can
+                //   use other rectangles to get segments of textures, useful for things such as spritesheets.
+                //   The locking mode. Basically, how you want to use the pixels. Since we're passing them to OpenGL,
+                //   we only need ReadOnly.
+                //   Next is the pixel format we want our pixels to be in. In this case, ARGB will suffice.
+                //   We have to fully qualify the name because OpenTK also has an enum named PixelFormat.
+                var data = image.LockBits(
+                    new Rectangle(0, 0, image.Width, image.Height),
+                    ImageLockMode.ReadOnly,
+                    System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+                // Now that our pixels are prepared, it's time to generate a texture. We do this with GL.TexImage2D.
+                // Arguments:
+                //   The type of texture we're generating. There are various different types of textures, but the only one we need right now is Texture2D.
+                //   Level of detail. We can use this to start from a smaller mipmap (if we want), but we don't need to do that, so leave it at 0.
+                //   Target format of the pixels. This is the format OpenGL will store our image with.
+                //   Width of the image
+                //   Height of the image.
+                //   Border of the image. This must always be 0; it's a legacy parameter that Khronos never got rid of.
+                //   The format of the pixels, explained above. Since we loaded the pixels as ARGB earlier, we need to use BGRA.
+                //   Data type of the pixels.
+                //   And finally, the actual pixels.
+                GL.TexImage2D(TextureTarget.Texture2D,
+                    0,
+                    PixelInternalFormat.Rgba,
+                    image.Width,
+                    image.Height,
+                    0,
+                    PixelFormat.Bgra,
+                    PixelType.UnsignedByte,
+                    data.Scan0);
+            }
+
+            // Now that our texture is loaded, we can set a few settings to affect how the image appears on rendering.
+
+            // First, we set the min and mag filter. These are used for when the texture is scaled down and up, respectively.
+            // Here, we use Linear for both. This means that OpenGL will try to blend pixels, meaning that textures scaled too far will look blurred.
+            // You could also use (amongst other options) Nearest, which just grabs the nearest pixel, which makes the texture look pixelated if scaled too far.
+            // NOTE: The default settings for both of these are LinearMipmap. If you leave these as default but don't generate mipmaps,
+            // your image will fail to render at all (usually resulting in pure black instead).
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+
+            GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+
+            return new Texture(handle);
+        }
+
+        public Texture(int glHandle)
+        {
+            Handle = glHandle;
+        }
+
+        // Activate texture
+        // Multiple textures can be bound, if your shader needs more than just one.
+        // If you want to do that, use GL.ActiveTexture to set which slot GL.BindTexture binds to.
+        // The OpenGL standard requires that there be at least 16, but there can be more depending on your graphics card.
+        public void Use(TextureUnit unit)
+        {
+            GL.ActiveTexture(unit);
+            GL.BindTexture(TextureTarget.Texture2D, Handle);
+        }
+    }
+}
